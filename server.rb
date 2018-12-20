de7835e68d4b28989bd1ac61caa57007ef73f41b
Kevin Vahdat <kvahdat@nyc.yearup.org>
2018-12-20 16:52:05 -0500

    Added post page

--- server.rb
+++ server.rb
@@ -7,10 +7,12 @@ require 'active_record'
 
 #require model classes
 # require './models/cake.rb'
+require './models/user.rb'
+require './models/post.rb'
 
 # Use `binding.pry` anywhere in this script for easy debugging
 require 'pry'
-require 'csv'
+
 
 # Connect to a sqlite3 database
 # If you feel like you need to reset it, simply delete the file sqlite makes
@@ -28,4 +30,23 @@ end
 
 get '/signup' do
   erb :signup
+end
+
+get '/login' do
+  erb :login
+end
+
+post '/newuser/signup' do
+  already_user = User.find_by(email: params["email"])
+  if already_user
+    redirect '/login'
+  else
+    User.create(first_name: params["first_name"], last_name: params["last_name"], email: params["email"], birthday: params["birthday"], password: params["password"])
+    session[:user_id] = already_user.id
+    redirect '/'
+  end
+end
+
+get '/user/feed' do
+  erb :blogfeed
 end
